eba552f4fd3659ca01885c56fc3adffd1098b87d
SonyaAnik <sonya.2003.02.17.93@gmail.com>
2023-01-11 10:20:54 +0600

    lab3_2

---
+++ ShipGame/ShipGame/DecisionTree/TreeCreation.cs
@@ -0,0 +1,44 @@
+﻿using Hwdtech;
+namespace ShipGame.DecisionTree
+{
+    public class TreeCreation : ICommand
+    {
+        private string file;
+        public TreeCreation(string file)
+        {
+            this.file = file;
+        }
+
+        public void Execute()
+        {
+            var decision = IoC.Resolve<Dictionary<int, object>>("Create.Tree");
+            try
+            {
+                using (StreamReader st = File.OpenText(file))
+                {
+                    string? line;
+                    while ((line = st.ReadLine()) != null)
+                    {
+                        var fill = line.Split().Select(c => Convert.ToInt32(c)).ToList();
+
+                        foreach (var x in fill)
+                        {
+                            decision.TryAdd(x, new Dictionary<int, object>());
+                            decision = (Dictionary<int, object>)decision[x];
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException x)
+            {
+                throw new FileNotFoundException(x.ToString());
+            }
+            catch (Exception x)
+            {
+                throw new Exception(x.ToString());
+            }
+
+        }
+    }
+}
+
---
+++ ShipGame/Tests/TestDecisionTree/TestTreeCreation.cs
@@ -0,0 +1,51 @@
+﻿using Hwdtech;
+using Moq;
+using Hwdtech.Ioc;
+using ShipGame.DecisionTree;
+using ShipGame.Move;
+
+namespace Tests.TestDecisionTree
+{
+    public class TestTreeCreation
+    {
+
+        Mock<IStrategy> DecisionStrategy = new Mock<IStrategy>();
+        public TestTreeCreation()
+        {
+            new InitScopeBasedIoCImplementationCommand().Execute();
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Create.Tree", (object[] args) => DecisionStrategy.Object.RunStrategy(args)).Execute();
+        }
+
+
+        [Test]
+        public void PositiveDecisionTreeTest()
+        {
+            string path = "../../../InitialData.txt";
+            DecisionStrategy.Setup(c => c.RunStrategy(It.IsAny<object[]>())).Returns(new Dictionary<int, object>()).Verifiable();
+            var mock = new TreeCreation(path);
+            mock.Execute();
+            DecisionStrategy.Verify();
+        }
+
+        [Test]
+        public void FileNotFoundException()
+        {
+            string path = "MyFile.txt";
+            DecisionStrategy.Setup(c => c.RunStrategy(It.IsAny<object[]>())).Returns(new Dictionary<int, object>()).Verifiable();
+            var mock = new TreeCreation(path);
+            Assert.Throws<FileNotFoundException>(() => mock.Execute());
+            DecisionStrategy.Verify();
+        }
+        
+        [Test]
+        public void Exception()
+        {
+            string path = "";
+            DecisionStrategy.Setup(c => c.RunStrategy(It.IsAny<object[]>())).Returns(new Dictionary<int, object>()).Verifiable();
+            var mock = new TreeCreation(path);
+            Assert.Throws<Exception>(() => mock.Execute());
+            DecisionStrategy.Verify();
+        }
+    }
+}
