d66057238979422f2f1cea2fdef58b0b44f98242
Nadi13 <nadia_yudintseva@mail.ru>
2023-05-21 13:56:15 +0600

    strategy

---
+++ ShipGame/ShipGame/Server/CreateWithStartThreadStrategy.cs
@@ -0,0 +1,26 @@
+﻿using Hwdtech;
+using ShipGame.Move;
+using ShipGame.Server;
+using System.Collections.Concurrent;
+
+namespace SpaceBattle.ServerStrategies
+{
+    public class CreateAndStartThreadStrategy : IStrategy
+    {
+        public object RunStrategy(params object[] args)
+        {
+            var senderDict = IoC.Resolve<ConcurrentDictionary<string, ISender>>("ThreadIDSenderMapping");
+            var sender = (ISender)args[1];
+            senderDict.TryAdd((string)args[0], sender);
+            if (args.Length > 3)
+            {
+                sender.Send(new ActionCommand((Action)args[3]));
+            }
+            var ST = new ServerThread((IReceiver)args[2]);
+            ST.Execute();
+            var threadDict = IoC.Resolve<ConcurrentDictionary<string, ServerThread>>("ThreadIDMyThreadMapping");
+            threadDict.TryAdd((string)args[0], ST);
+            return ST;
+        }
+    }
+}
