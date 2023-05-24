8647d1aef294d4709eba8edf151d9fab26b3f5e8
Nadi13 <nadia_yudintseva@mail.ru>
2023-05-24 20:51:30 +0600

    the end

--- ShipGame/ShipGame/Server/CreateWithStartThreadStrategy.cs
+++ ShipGame/ShipGame/Server/CreateWithStartThreadStrategy.cs
@@ -2,23 +2,26 @@
 using ShipGame.Move;
 using ShipGame.Server;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace SpaceBattle.ServerStrategies
 {
-    public class CreateAndStartThreadStrategy : IStrategy
+    public class CreateWithStartThreadStrategy : IStrategy
     {
         public object RunStrategy(params object[] args)
         {
             var senderDict = IoC.Resolve<ConcurrentDictionary<string, ISender>>("SenderDictionary");
-            var sender = (ISender)args[1];
-            senderDict.TryAdd((string)args[0], sender);
-            if (args.Length > 3)
-            {
-                sender.Send(new ActionCommand((Action)args[3]));
-            }
-            var ST = new ServerThread((IReceiver)args[2]);
-            ST.Start();
             var threadDict = IoC.Resolve<ConcurrentDictionary<string, ServerThread>>("ThreadDictionary");
+            BlockingCollection<ShipGame.Move.ICommand> queue = new BlockingCollection<ShipGame.Move.ICommand>(100);
+            var sender = new SenderAdapter(queue);
+            if (args.Length > 1)
+            {
+                sender.Send(new ActionCommand((Action)args[1]));
+            }
+            var receiver = new ReceiverAdapter(queue);
+            var ST = new ServerThread(receiver);
+            ST.Start();
+            senderDict.TryAdd((string)args[0], sender);
             threadDict.TryAdd((string)args[0], ST);
             return ST;
         }
