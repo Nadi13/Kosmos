bf387645d96c9aa0c35654a0e95bd23c79b501d2
Nadi13 <nadia_yudintseva@mail.ru>
2023-06-02 11:32:35 +0600

    test

---
+++ ShipGame/Tests/TestGameCommand/TestsGameCommand.cs
@@ -0,0 +1,15 @@
+﻿using Hwdtech.Ioc;
+using Hwdtech;
+using System.Collections.Concurrent;
+using Moq;
+using ShipGame.Game;
+using ICommand = ShipGame.Move.ICommand;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Tests.TestGameCommand
+{
+    internal class TestsGameCommand
+    {
+    }
+}
