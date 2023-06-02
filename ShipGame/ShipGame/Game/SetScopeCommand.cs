56741fc43958b5cec2debf1a1e860311af7c4902
Nadi13 <nadia_yudintseva@mail.ru>
2023-06-02 11:28:09 +0600

    set scope command

---
+++ ShipGame/ShipGame/Game/SetScopeCommand.cs
@@ -0,0 +1,27 @@
+﻿using ICommand = ShipGame.Move.ICommand;
+using Hwdtech;
+
+namespace ShipGame.Game
+{
+    public class SetScopeCommand: ICommand
+    {
+        string id;
+        GameCommand GameCommand;
+        object GameScope;
+        public SetScopeCommand(string id, object GameScope, Queue<ICommand> queue)
+        {
+            this.id = id;
+            this.GameCommand = new GameCommand(id, queue);
+            this.GameScope = GameScope;
+        }
+        public void Execute()
+        {
+            var InitialScope = IoC.Resolve<object>("ThreadScope.Current", id);
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", this.GameScope).Execute();
+
+            GameCommand.Execute();
+
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", InitialScope).Execute();
+        }
+    }
+}
