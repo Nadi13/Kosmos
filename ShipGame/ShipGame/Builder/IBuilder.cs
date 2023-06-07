da3d9c6a04052934cd58ccb1229b103df8cdad13
Nadi13 <nadia_yudintseva@mail.ru>
2023-06-07 12:31:26 +0600

    IBuilder and Strategy

---
+++ ShipGame/ShipGame/Builder/IBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipGame.Builder
+{
+    public interface IBuilder
+    {
+        IBuilder addAnything(string param, params object[] args);
+        object Build();
+    }
+}
